62ae04b9dbeb8a5bf865dc31d6c265294ea91fd5
Mohamed Emara <memaraaa123@gmail.com>
2025-10-14 05:00:57 +0300

    Updates

--- assets/HeroCamera-WdSnJaUo.js
+++ assets/HeroCamera-WdSnJaUo.js
@@ -1,1 +1,2 @@
 import{r as s,e as t,j as i,f as a}from"./vendor-DW9LxI3u.js";import"./three-BRZ5zbz7.js";import"./react-DzEnchs7.js";const f=({children:p})=>{const o=s.useRef();return t((r,e)=>{a.damp3(r.camera.position,[0,0,5.5],.25,e),a.dampE(o.current.rotation,[-r.pointer.y/3,r.pointer.x/2,0],.25,e)}),i.jsx("group",{ref:o,children:p})};export{f as default};
+//# sourceMappingURL=HeroCamera-WdSnJaUo.js.map
